ad3ae75e5e7798f63ac032e4210c1237c10373fb
zyang30 <zyang30.work@gmail.com>
2022-06-14 19:36:17 -0700

    first push

---
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.luv2code.ecommerce.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.luv2code.ecommerce.entity.Product;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin("http://localhost:4200")
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
