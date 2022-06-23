4ca1156a0c39bfd436ace97aa77582ed5188e541
zyang30 <zyang30.work@gmail.com>
2022-06-23 04:54:29 -0700

    update search keywords function

--- 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -12,6 +12,8 @@ export class ProductListComponent implements OnInit {
 
   products: Product[];
   currentCategoryId: number;
+  searchMode: boolean;
+
   constructor(private productService: ProductService,
     private route: ActivatedRoute) { }
 
@@ -24,24 +26,47 @@ export class ProductListComponent implements OnInit {
 
   listProducts() {
 
-    //check if "id" parameter is available
-    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+    this.searchMode = this.route.snapshot.paramMap.has('keyword');
 
-    if (hasCategoryId) {
-      //get the "id" param string. convert string to a number using the "+" symbol
-      //!(Non-null assertion operator) use for avoid null
-      this.currentCategoryId = +this.route.snapshot.paramMap.get('id')!;
-    } else {
-      //not category id available ... default to category id 1
-      this.currentCategoryId = 1;
+    if(this.searchMode) {
+      console.log("check");
+      this.handleSearchProducts();
+    }else{
+      this.handleListProducts();
     }
 
-    //now get the products for the given category id
-    this.productService.getProductList(this.currentCategoryId).subscribe(
+  }
+
+  handleSearchProducts() {
+    const theKeyword: string = this.route.snapshot.paramMap.get('keyword')!;
+
+    //now search for the products using keyword
+    this.productService.searchProducts(theKeyword).subscribe(
       data => {
         this.products = data;
       }
     )
+  }
+
+  handleListProducts(){
+        //check if "id" parameter is available
+        const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+
+        if (hasCategoryId) {
+          //get the "id" param string. convert string to a number using the "+" symbol
+          //!(Non-null assertion operator) use for avoid null
+          this.currentCategoryId = +this.route.snapshot.paramMap.get('id')!;
+        } else {
+          //not category id available ... default to category id 1
+          this.currentCategoryId = 1;
+        }
+    
+        //now get the products for the given category id
+        this.productService.getProductList(this.currentCategoryId).subscribe(
+          data => {
+            this.products = data;
+          }
+        )
   }
 
 }
--- 03-frontend/angular-ecommerce/src/app/services/product.service.ts
+++ 03-frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -20,6 +20,10 @@ export class ProductService {
     //build URL based on category id
     const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
 
+    return this.getProducts(searchUrl);
+  }
+
+  private getProducts(searchUrl: string): Observable<Product[]> {
     return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
       map(response => response._embedded.products)
     );
@@ -29,6 +33,12 @@ export class ProductService {
     return this.httpClient.get<GetResponseProductCategory>(this.categoryUrl).pipe(
       map(response => response._embedded.productCategory)
     );
+  }
+
+  searchProducts(theKeyword: string): Observable<Product[]> {
+        //build URL based on keyword
+        const searchUrl = `${this.baseUrl}/search/findByNameContaining?name=${theKeyword}`;
+        return this.getProducts(searchUrl);
   }
 }
 
