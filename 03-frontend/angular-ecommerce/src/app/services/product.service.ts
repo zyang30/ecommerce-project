702354e087476665ed9db2cbc1b8aa1252b620e7
zyang30 <zyang30.work@gmail.com>
2022-06-27 00:11:12 -0700

    update paginate

--- 03-frontend/angular-ecommerce/src/app/services/product.service.ts
+++ 03-frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -15,6 +15,24 @@ export class ProductService {
 
   constructor(private httpClient: HttpClient) { }
 
+  searchProductsPaginate(thePage: number,
+    thePageSize: number,
+    theKeyword: string): Observable<GetResponseProducts> {
+    const searchUrl = `${this.baseUrl}/search/findByNameContaining?name=${theKeyword}&page=${thePage}&size=${thePageSize}`;
+    return this.httpClient.get<GetResponseProducts>(searchUrl);
+  }
+
+  getProductListPaginate(thePage: number,
+    thePageSize: number,
+    theCategoryId: number): Observable<GetResponseProducts> {
+
+    //build URL based on category id
+    const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`
+      + `&page=${thePage}&size=${thePageSize}`;
+
+    return this.httpClient.get<GetResponseProducts>(searchUrl);
+  }
+
   getProductList(theCategoryId: number): Observable<Product[]> {
 
     //build URL based on category id
@@ -36,9 +54,9 @@ export class ProductService {
   }
 
   searchProducts(theKeyword: string): Observable<Product[]> {
-        //build URL based on keyword
-        const searchUrl = `${this.baseUrl}/search/findByNameContaining?name=${theKeyword}`;
-        return this.getProducts(searchUrl);
+    //build URL based on keyword
+    const searchUrl = `${this.baseUrl}/search/findByNameContaining?name=${theKeyword}`;
+    return this.getProducts(searchUrl);
   }
 
   getProduct(theProductId: number): Observable<Product> {
@@ -52,6 +70,12 @@ export class ProductService {
 interface GetResponseProducts {
   _embedded: {
     products: Product[];
+  }
+  page: {
+    size: number,
+    totalElements: number,
+    totalPages: number,
+    number: number
   }
 }
 
